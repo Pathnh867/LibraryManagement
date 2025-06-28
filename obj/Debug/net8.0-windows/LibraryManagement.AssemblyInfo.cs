3080662ba06e538a343a4e6def06132a149b4ca6
Pathnh867 <117632663+Pathnh867@users.noreply.github.com>
2025-06-28 14:35:47 +0700

    n

--- obj/Debug/net8.0-windows/LibraryManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/LibraryManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54255e8dbbc692abc4ece4124b63e3e4e011b997")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+396020bac298304322ac257f926357607238874f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
